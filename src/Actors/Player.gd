d63a8ad9be1c06e479cd848309adfa0489d8fa51
sharat36 <n.sharat.chandra36@gmail.com>
2020-08-03 02:57:20 +0530

    Added Rolling Motion

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -8,11 +8,17 @@ func _physics_process(delta: float) -> void:
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
-	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
-	)
-
+	var dir: = Vector2(0.0, 1.0)
+	
+	if Input.is_action_pressed("move_right"):
+		dir.x = 1.0
+	if Input.is_action_pressed("move_left"):
+		dir.x = -1.0
+	if Input.is_action_pressed("jump") and is_on_floor():
+		dir.y = -1.0
+	
+	return dir
+	
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2,
@@ -28,17 +34,21 @@ func calculate_move_velocity(
 		out.y = 0.0
 	return out
 func set_animation() -> void:
-	if(Input.is_action_pressed("move_right")):
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("roll"):
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("run")
-	elif(Input.is_action_pressed("move_left")):
+	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("roll"):
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("run")	
-	elif(Input.is_action_pressed("jump")):
+	elif Input.is_action_pressed("jump"):
 		$AnimatedSprite.play("jump")
-	elif(Input.is_action_pressed("roll")):
+	elif Input.is_action_pressed("roll") and Input.is_action_pressed("move_right"):
+		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("roll")
-	elif(Input.is_action_pressed("attack")):
+	elif Input.is_action_pressed("roll") and Input.is_action_pressed("move_left"):
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("roll")
+	elif Input.is_action_pressed("attack"):
 		$AnimatedSprite.play("attack")	
-	elif(is_on_floor()):
+	elif is_on_floor():
 		$AnimatedSprite.play("idle")
