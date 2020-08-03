0f1828b18a7c689febd7567e5378ba69a71890e0
sharat36 <n.sharat.chandra36@gmail.com>
2020-08-03 23:16:31 +0530

    Added Enemy Horizontal Movement

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,7 +1,7 @@
 extends Actor
 
 var is_attacking: bool = false
-var is_rolling: bool = false
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0 
 	var direction: = get_direction()
@@ -18,10 +18,6 @@ func get_direction() -> Vector2:
 		dir.x = -1.0
 	if Input.is_action_pressed("jump") and is_on_floor():
 		dir.y = -1.0
-	if Input.is_action_pressed("roll") and $AnimatedSprite.flip_h == false:
-		dir.x = 1.0
-	if Input.is_action_pressed("roll") and $AnimatedSprite.flip_h == true:
-		dir.x = -1.0
 		
 	return dir
 	
@@ -38,24 +34,31 @@ func calculate_move_velocity(
 		out.y = speed.y * direction.y
 	if is_jump_interrupted:
 		out.y = 0.0
-	if is_attacking or is_rolling:
+	if is_attacking:
 		out.x = 0.0
 	return out
 	
 func set_animation() -> void:
 	if Input.is_action_pressed("move_right") and not is_attacking:
 		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("run")
+		if Input.is_action_pressed("roll"):
+			$AnimatedSprite.play("roll")
+		elif Input.is_action_pressed("jump"):
+			$AnimatedSprite.play("jump")
+		else:
+			$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("move_left") and not is_attacking:
 		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play("run")	
+		if Input.is_action_pressed("roll"):
+			$AnimatedSprite.play("roll")
+		elif Input.is_action_pressed("jump"):
+			$AnimatedSprite.play("jump")
+		else:
+			$AnimatedSprite.play("run")	
 	elif Input.is_action_pressed("jump"):
 		$AnimatedSprite.play("jump")
 	elif Input.is_action_pressed("roll"):
 		$AnimatedSprite.play("roll")
-		is_rolling = true;
-	elif Input.is_action_just_released("roll"):
-		is_rolling = false;
 	elif Input.is_action_pressed("attack"):
 		$AttackArea/CollisionShape2D.disabled = false;
 		$AnimatedSprite.play("attack")	
@@ -63,7 +66,6 @@ func set_animation() -> void:
 	elif Input.is_action_just_released("attack"):
 		$AttackArea/CollisionShape2D.disabled = true;
 		is_attacking = false;
-	
 	elif is_on_floor():
 		$AnimatedSprite.play("idle")
 
--- src/Actors/Temp.gd
+++ src/Actors/Temp.gd
@@ -1,8 +1,50 @@
-extends Area2D
+extends "res://src/Actors/Actor.gd"
 
+onready var Player = get_parent().get_node("player")
 
+var vel = Vector2.ZERO
+var react_time = 400
+var dir = 0
+var next_dir = 0
+var next_dir_time = 0
+var next_jump_time = -1
 
+var target_player_dist = 100
+
+func set_dir(target_dir):
+	if next_dir != target_dir:
+		next_dir = target_dir
+		next_dir_time = OS.get_ticks_msec() + react_time
+
+func _physics_process(delta: float) -> void:
+	if Player.position.x < position.x - target_player_dist:
+		set_dir(-1)
+	elif Player.position.x > position.x + target_player_dist:
+		set_dir(1)
+	else:
+		set_dir(0)
+	
+	if OS.get_ticks_msec() > next_dir_time:
+		dir = next_dir
+	
+	if OS.get_ticks_msec() > next_jump_time and next_jump_time != -1 and is_on_floor():
+		if Player.position.y < position.y:
+			vel.y = -100
+		next_jump_time = -1
+	
+	vel.x = dir * 150;
+		
+	if Player.position.y < position.y and next_jump_time == -1:
+		next_jump_time = OS.get_ticks_msec() + react_time
+	
+	vel.y += max(gravity * delta, speed.y)
+	
+	if is_on_floor() and vel.y > 0:
+		vel.y = 0
+	
+	vel = move_and_slide(vel, FLOOR_NORMAL)
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
 	area.is_in_group("sword")
 	queue_free()
+
