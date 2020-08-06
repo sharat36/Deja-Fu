9279e38bb9617745069cd6f20516342447aa8a0e
sharat36 <n.sharat.chandra36@gmail.com>
2020-08-06 23:29:42 +0530

    Added Final Deja Fu

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -4,9 +4,13 @@ var is_attacking: bool = false
 var is_gripping: bool = false
 var dying: bool = false
 onready var Player = get_parent().get_node("player")
-var cur = 0
-var i = 1
-var pos = {}
+var pos = []
+var rewind_max_time = 6000
+var rewind_gap = 100
+var rewind_max_elems = rewind_max_time / rewind_gap
+var rewind_rate = 1.5
+var is_teleporting = false
+var teleport_time
 
 func _process(delta: float) -> void:
 	store_pos()
@@ -17,16 +21,16 @@ func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0 
 	var direction: = get_direction()
 	
-	if not check_teleport():
-		_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-		if not is_on_floor()  and ($LeftRayCast1.is_colliding() or $LeftRayCast2.is_colliding() or $RightRayCast1.is_colliding() or $RightRayCast2.is_colliding()):
-			_velocity.y = 100
-			is_gripping = true
-			$AnimatedSprite.play("wallgrip")
-		else:
-			is_gripping = false
-		_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-		set_animation()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	if not is_on_floor()  and ($LeftRayCast1.is_colliding() or $LeftRayCast2.is_colliding() or $RightRayCast1.is_colliding() or $RightRayCast2.is_colliding()):
+		_velocity.y = 100
+		is_gripping = true
+		$AnimatedSprite.play("wallgrip")
+	else:
+		is_gripping = false
+	set_animation()
+	check_teleport()
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
 	if dying:
@@ -79,25 +83,19 @@ func set_animation() -> void:
 	if Input.is_action_pressed("move_right") and not is_attacking:
 		$AnimatedSprite.flip_h = false
 		$AttackArea.scale = Vector2(1, 1)
-		if Input.is_action_pressed("roll"):
-			$AnimatedSprite.play("roll")
-		elif Input.is_action_pressed("jump"):
+		if Input.is_action_pressed("jump"):
 			$AnimatedSprite.play("jump")
 		else:
 			$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("move_left") and not is_attacking:
 		$AnimatedSprite.flip_h = true
 		$AttackArea.scale = Vector2(-1, 1)
-		if Input.is_action_pressed("roll"):
-			$AnimatedSprite.play("roll")
-		elif Input.is_action_pressed("jump"):
+		if Input.is_action_pressed("jump"):
 			$AnimatedSprite.play("jump")
 		else:
 			$AnimatedSprite.play("run")	
 	elif Input.is_action_pressed("jump"):
 		$AnimatedSprite.play("jump")
-	elif Input.is_action_pressed("roll"):
-		$AnimatedSprite.play("roll")
 	elif Input.is_action_pressed("attack"):
 		$AttackArea/CollisionShape2D.disabled = false;
 		$AnimatedSprite.play("attack")
@@ -109,34 +107,46 @@ func set_animation() -> void:
 		$AnimatedSprite.play("idle")
 
 func store_pos():
-	if i % 250 == 0 :
-		pos[cur] = Player.get_position()
-		cur += 1
-	i += 1
+	if not is_teleporting:
+		var cur = OS.get_ticks_msec()
+		
+		if pos.size() == 0 :
+			pos.append([cur, Player.position, _velocity])
+		elif cur - pos[pos.size() - 1][0] > rewind_gap :
+			if pos.size() > rewind_max_elems :
+				pos.remove(0)
+			pos.append([cur, Player.position, _velocity])
 
 func check_teleport():
-	if dying:
-		return
-	if(Input.is_action_pressed("teleport")):
-		print(i)
-		var cur = i / 250
-		print(cur)
-		if(cur > 0):
-			teleport(pos[cur - 1])
-			return true
-		else: 
-			return false
-	return false
+	if Input.is_action_pressed("teleport") and pos.size() > 0:
+		if not is_teleporting :
+			teleport_time = OS.get_ticks_msec()
+			
+		is_teleporting = true
+	
+		while(pos[pos.size() - 1][0] > teleport_time - rewind_rate * (OS.get_ticks_msec() - teleport_time) and pos.size() > 1):
+			show_clock(pos[pos.size() - 1])
+			pos.remove(pos.size() - 1)
+		if pos.size() == 1 :
+			teleport(pos[0])
 
+	else:
+		if is_teleporting :
+			teleport(pos[pos.size() - 1])
+		is_teleporting = false
+		$Clock.visible = false
+
+
+func show_clock(pos):
+	$Clock.position = pos[1] - Player.position
+	$Clock.visible = true
 func teleport(pos):
-	Player.position = pos
+	Player.position = pos[1]
+	_velocity = pos[2]
+	$Clock.visible = false
 
 func _on_PlayerArea_body_entered(body):
-	if dying:
-		return
 	if body.is_in_group("damage"):
 		dying = true
 		$AnimatedSprite.play("death")
-		$DeathAudio.play()
-		yield($AnimatedSprite, "animation_finished")
 		get_tree().reload_current_scene()
